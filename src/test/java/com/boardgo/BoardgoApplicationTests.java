4618e82f7cda3e00bcbd217050795092019d50e8
yeahdy <yejine98@naver.com>
2024-07-25 17:15:21 +0900

    Feat: 프로젝트 초기화 :

---
+++ src/test/java/com/boardgo/BoardgoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boardgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
