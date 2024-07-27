1a10c6f1f589e4ca727e005e35c46de007e570e0
yeahdy <yejine98@naver.com>
2024-07-27 23:54:40 +0900

    Feat: pre-commit 스크립트 작성 및 applcation 설정 추가
    - application.yml profile active 추가

---
+++ script/pre-commit.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# root directory
+MAIN_REPO_PATH=$(git rev-parse --show-toplevel)
+# submodule directory
+SUBMODULE_PATH="$MAIN_REPO_PATH/board-submodule"
+
+# 스테이징 영역에 있는 변경된 파일 불러오기
+targetFiles=$(git diff --staged --name-only | grep 'application')
+
+for file in $targetFiles; do
+  if [[ "$file" == *application* ]]; then
+    cp "$MAIN_REPO_PATH/src/main/resources/$file" "$SUBMODULE_PATH/backend/$file"
+
+    cd SUBMODULE_PATH
+    git add "backend/$file"
+    echo "Pre-commit $file for submodule"
+  fi
+done
+
+# commit & push in submodule repository
+git commit -m "Feat: update application files"
+git push origin main
+
+cd $MAIN_REPO_PATH
