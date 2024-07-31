bb4395d77f07edfabf982e999b60e6319b49fe55
yeahdy <yejine98@naver.com>
2024-07-31 14:21:24 +0900

    Fix: spotless google format 옵션 추가 및 테스트

--- src/main/java/com/boardgo/BoardgoApplication.java
+++ src/main/java/com/boardgo/BoardgoApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BoardgoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BoardgoApplication.class, args);
-	}
-
+public static void main(String[] args) {
+	SpringApplication.run(BoardgoApplication.class, args);
+}
 }
