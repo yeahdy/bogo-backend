66e09b7f245e54e2961bd5f1c8b5043cf5f20e4c
ambosing <ambosing_@naver.com>
2024-08-02 19:15:45 +0900

    Fix: @Repository 삭제

--- src/main/java/com/boardgo/domain/user/repository/UserPrTagJdbcRepositoryImpl.java
+++ src/main/java/com/boardgo/domain/user/repository/UserPrTagJdbcRepositoryImpl.java
@@ -6,11 +6,9 @@ import java.util.List;
 
 import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.stereotype.Repository;
 
 import lombok.RequiredArgsConstructor;
 
-@Repository
 @RequiredArgsConstructor
 public class UserPrTagJdbcRepositoryImpl implements UserPrTagJdbcRepository {
 
