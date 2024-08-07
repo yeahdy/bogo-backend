895965b1c0008957d48c26b989765b56faf93010
Yang JiWon <ambosing_@naver.com>
2024-08-07 14:38:21 +0900

    Fix: SecurityConfig webSecurityCustomizer 추가 (#31)
    Signed-off-by: ambosing <ambosing_@naver.com>

--- src/main/java/com/boardgo/config/SecurityConfig.java
+++ src/main/java/com/boardgo/config/SecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -102,5 +103,19 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web ->
+                web.ignoring()
+                        .requestMatchers(
+                                "/lib/**",
+                                "/resources/**",
+                                "/static/**",
+                                "/css/**",
+                                "/js/**",
+                                "/img/**",
+                                "/src/docs/**");
     }
 }
