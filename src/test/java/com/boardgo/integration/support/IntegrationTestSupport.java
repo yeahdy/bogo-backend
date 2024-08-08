baf0ea49afba88eee15ee6c33f8839e31d8527d5
Yang JiWon <ambosing_@naver.com>
2024-08-08 18:32:53 +0900

    Fix: Test 데이터 시작 전 제거 (#34)
    * Fix: Test 데이터 시작 전 제거
    
    Signed-off-by: ambosing <ambosing_@naver.com>
    
    * Feat: submodule update
    
    Signed-off-by: ambosing <ambosing_@naver.com>
    
    ---------
    
    Signed-off-by: ambosing <ambosing_@naver.com>

--- src/test/java/com/boardgo/integration/support/IntegrationTestSupport.java
+++ src/test/java/com/boardgo/integration/support/IntegrationTestSupport.java
@@ -1,10 +1,12 @@
 package com.boardgo.integration.support;
 
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 @Transactional
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_CLASS)
 @ActiveProfiles("test")
 public abstract class IntegrationTestSupport {}
